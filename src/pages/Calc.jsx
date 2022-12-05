f6d2eff0fe9ced41e9d3b45894cfbda77d26ddfd
JonFactor <Jon.Factor2@gmail.com>
2022-12-05 13:18:24 -0500

    gay

--- src/pages/Calc.jsx
+++ src/pages/Calc.jsx
@@ -13,33 +13,43 @@ import { SimpleFooter } from "@/widgets/layout";
 import { useState } from "react";
 export function Calc() {
 
-  const [name, setName] = useState('');
-  const [mail, setMail] = useState('');
-  const [Pass, setPass] = useState('');
+  const [Price, setPrice] = useState('');
+  const [Down, setDown] = useState('');
+  const [Length, setLength] = useState('');
+  const [credit, setCredit] = useState('');
+  
+  
+  const sub = async () => {
 
-  const test = async () => {
-
-    console.log(name, mail, Pass)
+    inputs.className = "collapse"
 
   }
+
   
+  const back = async () => {
+
+    outputs.className = "collapse"
+    inputs.className = "visible"
+
+  }
+
   return (
     <>
       <img
-        src="img\Wallpapers\Calcs.jpg"
+        src="img\Wallpapers\ferran-fusalba-rosello-WgUHuGSWPVM-unsplash.jpg"
         className="absolute inset-0 z-0 h-full w-full object-cover"
       />
       <div className="absolute inset-0 z-0 h-full w-full bg-black/50" />
       <div className="container mx-auto p-4">
         
-      <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4">
+      <Card className="absolute top-2/4 left-2/4 w-full max-w-[42rem] -translate-y-2/4 -translate-x-2/4" id = "inputs">
           <CardHeader
             variant="gradient"
-            color="gray"
+            color="green"
             className="mb-4 grid h-28 place-items-center"
           >
             <Typography variant="h3" color="light">
-              Claculator
+              Calculator
             </Typography>
           </CardHeader>
           <CardBody className="flex flex-col gap-4">
@@ -51,8 +61,34 @@ export function Calc() {
 
           </CardBody>
           <CardFooter className="pt-0">
-            <Button variant="gradient" fullWidth color="gray">
-              Submit
+            <Button variant="gradient" fullWidth color="green" onClick={sub}>
+            Calculate
+            </Button>
+            
+          </CardFooter>
+        </Card>
+
+        <Card className="absolute top-2/4 left-2/4 w-full max-w-[42rem] -translate-y-2/4 -translate-x-2/4" id = "outputs">
+          <CardHeader
+            variant="gradient"
+            color="green"
+            className="mb-4 grid h-28 place-items-center"
+          >
+            <Typography variant="h3" color="light">
+              Results
+            </Typography>
+          </CardHeader>
+          <CardBody className="flex flex-col gap-4">
+
+            <Input variant="standard"  label="Car Price" size="lg" />
+            <Input variant="standard" label="Down Payment"size="lg" />
+            <Input variant="standard" label="Length (months)"size="lg" />
+            <Input variant="standard" label="Creidt Score"size="lg" />
+
+          </CardBody>
+          <CardFooter className="pt-0">
+            <Button variant="gradient" fullWidth color="green" onClick={back}>
+            back
             </Button>
             
           </CardFooter>
@@ -67,3 +103,5 @@ export function Calc() {
 }
 
 export default Calc;
+
+// 
