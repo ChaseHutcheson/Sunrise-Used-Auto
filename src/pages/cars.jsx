d2be6ae91dcca95d219ab468b9395a6e6fa77d06
JonFactor <Jon.Factor2@gmail.com>
2022-12-07 13:21:15 -0500

    gay

--- src/pages/cars.jsx
+++ src/pages/cars.jsx
@@ -13,6 +13,14 @@ import carData from "@/data/car-data";
 import { Footer, SimpleFooter } from "@/widgets/layout";
 
 export function Cars() {
+  const LoggedIn = 1
+  const PostBox = <h1></h1>
+  const clickedthingy = 2
+  const Post = async () => {clickedthingy = 1}
+
+
+ 
+
   return (
     <>
       <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
@@ -31,10 +39,15 @@ export function Cars() {
             </div>
           </div>
         </div>
+        
       </div>
+      
       <section className="-mt-96 bg-blue-50 px-4 pt-20 pb-48">
+        
         <div className="container mx-auto">
+          
           <div className=" -mt-36 grid grid-cols-2 gap-5 gap-x-30 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+          
             {carData.map(({ img, color, title, price, description }) => (
               <CarsCard
                 img = {img}
@@ -45,6 +58,11 @@ export function Cars() {
               />
             ))}
           </div>
+          
+          {LoggedIn == 1 && 
+            <button className=" mt-10 h-2/5 w-1/15 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={Post}>Post</button>
+          }
+          {PostBox}
         </div>
       </section>
       <div className="bg-blue-gray-50/50">
